2941b3537f0b30059fe0fd11a9e95e4decd0eaf9
Parome15 <romeromesquidapau@gmail.com>
2018-01-04 09:19:14 +0100

    Inicio proyecto JSON 04/01/2018

---
+++ readgooglemapsjs.js
@@ -0,0 +1,23 @@
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'google_maps.json', true); 
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var responseText = xobj.responseText;
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(responseText);
+            callback(actual_JSON);
+        }
+    };
+    xobj.send(null);  
+}
+
+function viewJSON() {
+    loadJSON(function(response) {
+        /* Aqui l'objecte response representa l'objecte JSON que ens 
+           ha retornat el servidor */
+        document.getElementById("results").innerHTML = response.results[0].name;
+    });
+}
+
