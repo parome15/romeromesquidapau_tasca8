b0192afdb846eb98b700af52860ac1ce2d8b0d33
Parome15 <romeromesquidapau@gmail.com>
2018-01-08 13:57:20 +0100

    Arreglos en el funcionamientro de function viewJSON  (02/01/2018)

--- readgooglemapsjs.js
+++ readgooglemapsjs.js
@@ -12,12 +12,24 @@ function loadJSON(callback) {
     };
     xobj.send(null);  
 }
-
 function viewJSON() {
     loadJSON(function(response) {
+        
         /* Aqui l'objecte response representa l'objecte JSON que ens 
            ha retornat el servidor */
-        document.getElementById("results").innerHTML = response.results[0].name;
+        var str = "<table><tr><th>Nombre</th><th>Direccion</th><th>Latitud</th><th>Longitud</th><th>Tipo de restaurante</th><th>Icono</th>";
+        for(var i= 0; response.resluts.length; i++){
+            str +=
+            "<tr><td>"+response.results[i].name+
+            "</tr><td>"+response.results[i].vicinity+
+            "</tr><td>"+response.results[i].geometry.location.lat+
+            "</tr><td>"+response.results[i].geometry.location.lng+
+            "</tr><td>"+response.results[i].types+
+            "</tr><td><img src='"+response.results[i].icon+"'></td></tr>";
+        }
+        str += "</table>";
+        
+        document.getElementById("results").innerHTML = str;
     });
 }
 
