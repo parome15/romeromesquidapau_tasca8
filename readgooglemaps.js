d0fa31b859b6cfe021f8608d3262317f18dad5fc
Parome15 <romeromesquidapau@gmail.com>
2018-01-11 13:00:11 +0100

    Canvis en el CSS i comentaris (11/01/2018)

--- readgooglemaps.js
+++ readgooglemaps.js
@@ -11,12 +11,17 @@ function loadJSON(callback) {
         }
     };
     xobj.send(null);  
+
+    //Aixo es el codi donat per el profesor.
 }
 function viewJSON() {
     loadJSON(function(response) {
         
-        /* Aqui l'objecte response representa l'objecte JSON que ens 
-           ha retornat el servidor */
+        /*
+        Aqui preparam la variable str per que sigui un string que ens retorni la nostra taula.
+        Afegint el for que recor el JSON ficam dins cada celda la variable del JSON dins la nostra variable
+        str que es la nostra taula.
+        */
         var str = "<table><tr><th>Nombre</th><th>Direccion</th><th>Latitud</th><th>Longitud</th><th>Tipo de restaurante</th><th>Icono</th>";
         for(var i= 0; i<response.results.length; i++){
             str +=
@@ -29,6 +34,7 @@ function viewJSON() {
         }
         str += "</table>";
         
+        //Aqui el que feim es retornar la nostra taula a la pagina web.
         document.getElementById("results").innerHTML = str;
     });
 }
